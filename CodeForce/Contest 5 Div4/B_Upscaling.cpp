0f9df62e5205aca0e3ae6406251b54cbfd4ff5c2
DurjoyKumar177 <durjoykumar177@gmail.com>
2025-05-02 23:36:42 +0600

    codeforce contest code for div 4

---
+++ CodeForce/Contest 5 Div4/B_Upscaling.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string mat[n * 2][n * 2];
+
+        for (int i = 0; i < n ; i++)
+        {
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (j % 2 == 0)
+                    {
+                        mat[i][j] = "##";
+                        mat[i + 1][j] = "##";
+                    }
+                    else
+                    {
+                        mat[i][j] = "..";
+                        mat[i + 1][j] = "..";
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n * 2; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout << mat[i][j];
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
